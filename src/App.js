a60481d205a4fa44227b3f4fd1f796466e4963ae
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-21 14:30:25 -0400

    basic foundation for header and nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Navigation";
 
 function App() {
   return (
     <div>
+      <Header>
+        <Nav></Nav>
+      </Header>
       <main></main>
     </div>
   );
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <ul className="flex-row">
+        <li>
+          <a href="#about">About me</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+        <li>
+          <a href="#resume">Resume</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
