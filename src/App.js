ab9161bf22d4579e11dc47b884ac2051b347c60e
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-25 17:58:43 -0400

    create cards for projects

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import Header from "./components/Header";
-import Nav from "./components/Navigation";
 import Project from "./components/Project";
 
 function App() {
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,7 +1,54 @@
 import React from "react";
 
 function Project() {
-  return <section></section>;
+  const projects = [
+    {
+      title: "Timed-Quiz-Game",
+      technologies: "HTML/CSS/JS",
+      app: "https://grayad.github.io/timed-quiz-game/",
+      github: "https://github.com/grayad/timed-quiz-game",
+    },
+    {
+      title: "Weather-Retriever",
+      technologies: "HTML/CSS/JS/Server-Side API",
+      app: "https://grayad.github.io/weather-retriever/",
+      github: "https://github.com/grayad/weather-retriever",
+    },
+    {
+      title: "Habit-Rabbit",
+      technologies: "Full Stack Application: HTML/CSS/JS/SQL/Node/Express",
+      app: "https://habit--rabbit.herokuapp.com/",
+      github: "https://github.com/grayad/habit-rabbit",
+    },
+  ];
+  return (
+    <section>
+      <div className="d-flex flex-row">
+        {projects.map((project) => (
+          <div className="card" key={project.title}>
+            <img
+            //   src={require(`../../assets/${project.title}.png`)}
+              class="card-img-top"
+              alt="Application Screenshot"
+            ></img>
+            <div class="card-body">
+              <h5 class="card-title">{project.title}</h5>
+              <p class="card-text">
+                This application was built using the following:{" "}
+                {project.technologies}
+              </p>
+              <a href={project.app} class="btn btn-primary">
+                Go to Application
+              </a>
+              <a href={project.github} class="btn btn-primary">
+                Go to Github
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
 export default Project;
