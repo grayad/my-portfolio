0285f3fb2ca2d33259cfa4a0d1aed03c6d50bbec
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-26 10:32:14 -0400

    add About and Contact Form to the App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./components/Header";
+import About from "./components/pages/About";
 import Project from "./components/Project";
+import Contact from "./components/pages/Contact";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
+        <About></About>
+        <Contact></Contact>
         <Project></Project>
       </main>
     </div>
