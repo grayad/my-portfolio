2a118df6a865f75b84426fd6ff6481dc11e5045e
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-08-26 10:51:03 -0400

    initialize contact form state

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,21 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Contact() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
   return (
     <section>
       <h1>Contact Me</h1>
       <form id="contact-form">
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" name="name" />
+          <input type="text" name="name" defaultValue={name} />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" name="email" />
+          <input type="email" name="email" defaultValue={email} />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" rows="5" />
+          <textarea name="message" rows="5" defaultValue={message} />
         </div>
         <button type="submit">Submit</button>
       </form>
