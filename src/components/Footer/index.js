d67ab6cf6308f80b47e53c73bc50bc433139fe9d
Alexus Gray <alexus_gray@alumni.unc.edu>
2022-10-04 16:03:42 -0400

    fix github commits post gh pages deploy

---
+++ src/components/Footer/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+
+function Footer() {
+  return (
+    <footer className="d-flex align-items-center justify-content-center p-1">
+      <a href="https://github.com/grayad/" class='icon'>
+        <FaGithub size={40}></FaGithub>
+      </a>
+      <a href="https://www.linkedin.com/in/alexus-lexi-gray-capm-151434179/" class='icon'>
+        <FaLinkedinIn size={40}></FaLinkedinIn>
+      </a>
+      <a href="mailto: alexus_gray@alumni.unc.edu" class='icon'>
+        <AiOutlineMail size={40}></AiOutlineMail>
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { validateEmail } from "../../utils/helpers";
+
+export default function Contact() {
+  // initialize form state
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+
+  //   useState for error messages
+  const [errorMessage, setErrorMessage] = useState("");
+
+  //   sync form data to the state of the component (any change in input can be captured in formState)
+  function handleChange(e) {
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+    }
+  }
+
+  //   submit form (backend later)
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
+  // JSX
+  return (
+    <section class='container'>
+      <h2>Contact Me</h2>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="Name"
+            defaultValue={name}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onBlur={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="Message"
+            rows="5"
+            defaultValue={message}
+            onBlur={handleChange}
+          />
+        </div>
+        {/* conditional statement: if there is an error, display the message */}
+        {errorMessage && (
+          <div>
+            <p className="error-text">{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
