435b4dd6aed7d0f7becc7ac9b65396eb8c70dfae
Alexus Gray <alexus_gray@alumni.unc.edu>
2023-04-28 16:59:50 -0400

    update icon colors and resume button to match theme

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -13,42 +13,42 @@ export default function Resume() {
       <div class="pb-5">
         <h2>Resume</h2>
         <a href="./assets/AlexusGray_Resume.pdf" className="fs-4 text" download>
-          <button className="btn btn-light">Download Resume</button>
+          <button className="btn bg-light">Download Resume</button>
         </a>
       </div>
       <div>
         <h3 class="text-center">Coding Proficiencies</h3>
         <ul class="row d-flex align-items-center justify-content-center">
-          <li class="col-5">
-            <AiOutlineHtml5 size={40} color="#f7f7f7"></AiOutlineHtml5>HTML
+          <li class="col-5 icon">
+            <AiOutlineHtml5 size={40} color="#87BFC2"></AiOutlineHtml5>HTML
           </li>
           <li class="col-5">
-            <TbBrandCss3 size={40} color="#f7f7f7"></TbBrandCss3>CSS
+            <TbBrandCss3 size={40} color="#87BFC2"></TbBrandCss3>CSS
           </li>
           <li class="col-5">
-            <ImGit size={40} color="#f7f7f7"></ImGit> Git
+            <ImGit size={40} color="#87BFC2"></ImGit> Git
           </li>
           <li class="col-5">
-            <TbBrandJavascript size={40} color="#f7f7f7"></TbBrandJavascript>
+            <TbBrandJavascript size={40} color="#87BFC2"></TbBrandJavascript>
             JavaScript
           </li>
           <li class="col-5">
-            <IoLogoNodejs size={40} color="#f7f7f7"></IoLogoNodejs>Node.js
+            <IoLogoNodejs size={40} color="#87BFC2"></IoLogoNodejs>Node.js
           </li>
           <li class="col-5">
-            <SiExpress size={40} color="#f7f7f7"></SiExpress> Express.js
+            <SiExpress size={40} color="#87BFC2"></SiExpress> Express.js
           </li>
           <li class="col-5">
-            <GrMysql size={40} color="#f7f7f7"></GrMysql>MySQL
+            <GrMysql size={40} color="#87BFC2"></GrMysql>MySQL
           </li>
           <li class="col-5">
-            <SiSequelize size={40} color="#f7f7f7"></SiSequelize> Sequelize
+            <SiSequelize size={40} color="#87BFC2"></SiSequelize> Sequelize
           </li>
           <li class="col-5">
-            <SiMongodb size={40} color="#f7f7f7"></SiMongodb>MongoDB
+            <SiMongodb size={40} color="#87BFC2"></SiMongodb>MongoDB
           </li>
           <li class="col-5">
-            <GrReactjs size={40} color="#f7f7f7"></GrReactjs> React
+            <GrReactjs size={40} color="#87BFC2"></GrReactjs> React
           </li>
         </ul>
       </div>
